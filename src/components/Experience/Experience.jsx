557907f6fe0a09713da5c96ab22b92430e76e87f
suryamanoj07 <manojsurya463@gmail.com>
2025-07-12 11:39:47 +0530

    bug removed

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -30,7 +30,7 @@ const Experience = () => {
             }`}
           >
             {/* Timeline Circle */}
-            <div className="absolute sm:left-1/2 left-0 transform -translate-x-1/2 bg-gray-400 border-4 border-[#8245ec] w-12 h-12 sm:w-16 sm:h-16 rounded-full flex justify-center items-center z-10">
+            <div className="md:hidden absolute sm:left-1/2 left-0 transform -translate-x-1/2 bg-gray-400 border-4 border-[#8245ec] w-12 h-12 sm:w-16 sm:h-16 rounded-full flex justify-center items-center z-10">
               <img
                 src={experience.img}
                 alt={experience.company}
